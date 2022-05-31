db6d7cb431a86f5c0217a27bab17b4c83e2a5ef3
analivia1104 <analivia1104@gmail.com>
2022-05-31 16:14:29 -0300

    Enivo do uso do throw

--- code.js
+++ code.js
@@ -6,18 +6,23 @@
         var fr=new FileReader(); //Leitor de arquivo
 
     try{ // verifica se o arquivo está la
+        
         fr.readAsText(arquivo.files[0]);
 
         fr.onload=function(){ // abre o arquivo
         info = fr.result;
         document.getElementById('output').textContent=fr.result; // mostra o arquivo na pagina
-            }
+                            }
+
+        throw "Não foi, pois gripaloei no dia do vosso convite"
+        
         }
+
     catch (erro) { // mensagem de alerta
         console.log(erro);
         alert("Arquivo não localizado");
 
-        }
+                 }
 
     finally { // SEMPRE acontece
         alert("DS melhor turma!!! ");
