7a6d5d0899157e7323ed15411be446b1f311d680
analivia1104 <analivia110405@gmail.com>
2022-06-04 17:33:45 -0300

    Envio das alterações na segunda coluna

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
-    /* trocas as coisas de ingles p/ portugues aqui e no html */
-    
-    const inputFileButton = document.getElementById("inputfilebutton"); // declaração da ação
-    inputFileButton.addEventListener("click", EscolherArquivo);
+    const EntradaArquivoBotao = document.getElementById("entradaarquivobotao"); // declaração da ação
+    EntradaArquivoBotao.addEventListener("click", EscolherArquivo);
 
     const EntradaNumeroBotao = document.getElementById("entradanumerobotao"); // declaração da ação
     EntradaNumeroBotao.addEventListener("click", EscreverNumero);
@@ -13,44 +11,51 @@
     try{ // verifica se o arquivo está la
         
         fr.readAsText(arquivo.files[0]);
-
         fr.onload=function(){ // abre o arquivo
+
         info = fr.result;
-        document.getElementById('output').textContent=fr.result; // mostra o arquivo na pagina
+        document.getElementById('saidaarquivo').textContent=fr.result; // mostra o arquivo na pagina
                             }
-
-        var info = document.getElementById('output').textContent;
-
-        console.log(info);
-        
-        if (info == null) {
-        throw "Há um arquivo vazio!";
-        }
-        
+        var info = document.getElementById('saidaarquivo').textContent;
+        if (info == "") throw "Há um arquivo vazio!";
         }
 
     catch (erro) { // mensagem de alerta
-        console.log(erro);
         alert("Arquivo não localizado");
-
                  }
 
     finally { // SEMPRE acontece
-        alert("DS melhor turma!!! ");
-
+        alert("Obrigada pela visita!");
             }   
                               }
-    function EscreverNumero (){
 
-    numero = document.getElementById("entradanumero").value
+    function EscreverNumero (){
+        numero = document.getElementById("entradanumero").value
 
     try {
         if (numero == '') throw 'Por favor, informe um numero!'
-    }
+        }
 
     catch (erro){
-        console.log (numero)
         document.getElementById("saidanumero").innerHTML = erro //verificando se está vazio 
 
-    }
+                }
+
+
+    try{
+        if (numero < 5 || numero >10) // verifica se o numero está entre 5 e 10
+        throw ' Por favor, informe um numero entre 5 a 10!'
+
+        }
+
+    catch (erro){
+        document.getElementById("saidanumero").innerHTML = erro
+
+                }
+
+    finally{
+        alert("O numero inserido foi: " + numero)
+
+            }
+
                                 }
